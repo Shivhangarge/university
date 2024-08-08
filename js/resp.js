c8bdac2e22a1ed495afbb299c5936bf7a3417d03
shivlila7620@gmail.com <shivlila7620@gmail.com>
2024-08-08 12:02:46 +0530

    Initial commit

---
+++ js/resp.js
@@ -0,0 +1,13 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+rightList=document.querySelector('.nav-list')
+rightNav=document.querySelector('.rightNav')
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('v-class-resp');
+
+
+
+})
